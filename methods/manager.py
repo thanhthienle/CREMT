f86bcc415b46f782cde238b3b3fee12a3eb65761
MadridMaverick <thienlt.7@gmail.com>
2023-07-11 17:40:06 +0000

    change seeds

--- methods/manager.py
+++ methods/manager.py
@@ -582,10 +582,10 @@ class Manager(object):
             self.replayed_data = [[] for e_id in range(args.replay_epochs)]
 
             # random seed
-            random.seed(args.seed + seed_id*100)
+            random.seed(args.seed + seed_id*300)
 
             # sampler
-            sampler = data_sampler(args=args, seed=args.seed + seed_id*100)
+            sampler = data_sampler(args=args, seed=args.seed + seed_id*300)
             self.rel2id = sampler.rel2id
             self.id2rel = sampler.id2rel
 
