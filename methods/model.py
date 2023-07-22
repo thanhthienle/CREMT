17090f6f5b5853e129bb87c0d85fdaf45d2235bf
MadridMaverick <thienlt.7@gmail.com>
2023-07-22 11:00:15 +0000

    add ETF

--- methods/model.py
+++ methods/model.py
@@ -1,18 +1,51 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
+import numpy as np
 from .backbone import BertRelationEncoder
+
+
+class ETFLinear(nn.Module):
+    def __init__(self, feat_in, feat_out, device='cuda', dtype=torch.float32):
+        super().__init__()
+        P = self._generate_random_orthogonal_matrix(feat_in, feat_out)
+        I = torch.eye(feat_out)
+        one = torch.ones(feat_out, feat_out)
+        M = np.sqrt(feat_out / (feat_out-1)) * \
+            torch.matmul(P, I-((1/feat_out) * one))
+        self.M = M.to(device)
+    
+        self.InstanceNorm = nn.InstanceNorm1d(feat_in, affine=False, device=device)
+        self.BatchNorm = nn.BatchNorm1d(feat_in, affine=False, device=device)
+
+    def _generate_random_orthogonal_matrix(self, feat_in, feat_out):
+        a = np.random.random(size=(feat_in, feat_out))
+        P, _ = np.linalg.qr(a)  # This function returns an orthonormal matrix (q) and an upper-triangle matrix r(q)
+        P = torch.tensor(P).float()
+        assert torch.allclose(torch.matmul(P.T, P), torch.eye(feat_out), atol=1e-07), \
+            torch.max(torch.abs(torch.matmul(P.T, P) - torch.eye(feat_out)))
+        return P
+
+    def forward(self, x):
+        if x.shape[0] != 1:
+            return self.BatchNorm(x) @ self.M
+        else:
+            return self.InstanceNorm(x) @ self.M
 
 
 class Classifier(nn.Module):
     def __init__(self, args):
         super().__init__()
+        if args.ETF:
+            final_linear = ETFLinear(args.encoder_output_size, args.rel_per_task * args.num_tasks)
+        else: final_linear = nn.Linear(args.encoder_output_size, args.rel_per_task * args.num_tasks)
+
         self.head = nn.Sequential(
             nn.Linear(args.encoder_output_size * 2, args.encoder_output_size, bias=True),
             nn.ReLU(inplace=True),
             nn.Linear(args.encoder_output_size, args.encoder_output_size, bias=True),
             nn.ReLU(inplace=True),
-            nn.Linear(args.encoder_output_size, args.rel_per_task * args.num_tasks),
+            final_linear,
         ).to(args.device)
 
     def forward(self, x):
