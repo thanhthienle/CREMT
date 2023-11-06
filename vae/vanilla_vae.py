1b6d7ae7f84f00bbb9d5c2ef3b69df289a4d0e23
MadridMaverick <thienlt.7@gmail.com>
2023-11-06 15:26:53 +0700

    finish debug really

--- vae/vanilla_vae.py
+++ vae/vanilla_vae.py
@@ -152,7 +152,7 @@ class VanillaVAE(BaseVAE):
         z = z.to(current_device)
 
         samples = self.decode(z)
-        return [samples.numpy()]
+        return [samples.detach().cpu().numpy()]
 
     def generate(self, x: Tensor, **kwargs) -> Tensor:
         """
